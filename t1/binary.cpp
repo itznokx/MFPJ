170c7179e03061871cf3005239d489c30ea5357d
itznokx <dimitri.b.medeiros@gmail.com>
2024-12-02 10:16:34 -0300

    t

--- t1/binary.cpp
+++ t1/binary.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <tuple>
 using namespace std;
 // Validation Binary functions
 bool validationChar (char c){
@@ -43,6 +43,7 @@ string nextBin (string binary){
 	}
 	return binary;
 }
+// Inverse Binary function
 string inverseBin (string binary){
 	for (int i = 0;i<binary.length();i++){
 		if (binary[i] == '0')
@@ -52,7 +53,50 @@ string inverseBin (string binary){
 	}
 	return binary;
 }
+// 2's complement functions
 string twosComplement (string binary){
 	string aux = inverseBin(binary);
 	return nextBin(aux);
+}
+// Binary operations
+char andChar (char c1,char c2){
+	if (c1=='1'&&c2=='1')
+		return '1';
+	else
+		return '0';
+}
+char orChar (char c1,char c2){
+	if (c1=='0'&&c2=='0')
+		return '0';
+	else
+		return '1';
+}
+char xorChar(char c1,char c2){
+	if (c1==c2)
+		return '0';
+	else
+		return '1';
+}
+// Mathematical Binary Operations
+tuple<char,int> sumAux (char c1,char c2,int v1){
+	if (c1 == '1' && c2 == '1'){
+		if (v1 == 1)
+			return {'1',1};
+		else
+			return {'0',1};
+	}
+	if (c1=='0' && c2 == '0'){
+		if (v1 == 1)
+			return {'1',0};
+		else
+			return {'0',0};
+	}
+	if (v1 == 1)
+		return {'0',1};
+	else
+		return {'1',0};
+}
+string sumBin (string n1,string n2){
+	string aux = "";
+	return aux;
 }
