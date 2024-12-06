b11f5896574b293937b4a11b6e66b33c420f2d87
itznokx <dimitri.b.medeiros@gmail.com>
2024-12-06 15:08:21 -0300

    main1

--- t1/main1.cpp
+++ t1/main1.cpp
@@ -15,7 +15,6 @@ int main (void){
 	// Sucessor test
 	//cout << nextBin(number) << endl;
 	//cout << inverseBin (number) << endl;
-	auto [s1,s2] = equalizeN(number1,number2);
 	cout << s1 << " +" << endl << s2 << endl;
 	cout << sumBin(number1,number2) << endl ;
 	return 0;
