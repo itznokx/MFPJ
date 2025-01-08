821002c63af83e48581f4cd48a7a8bd1dd94f990
Dimitri Medeiros <79457494+itznokx@users.noreply.github.com>
2025-01-08 14:59:32 -0300

    Create reaction.js

---
+++ t2/reaction.js
@@ -0,0 +1,178 @@
+class Vec2{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.pos = null;
+    this.cor = null;
+  }
+  render (){
+    if (this.cor != null)
+      colore(this.cor[0],this.cor[1],this.cor[2])
+    if (this.pos == null)
+      seta(0,0,this.x,this.y)
+    else
+      seta(this.pos.x,
+           this.pos.y,
+           this.pos.x+this.x,
+           this.pos.y+this.y);
+  }
+  mult (k){
+    return new Vec2(this.x*k,this.y*k);
+  }
+  // Produto Escalar (dot product)
+  dot (v2){
+    return (this.x*v2.x+this.y+v2.y);
+  }
+  size(){
+    return sqrt(this.dot(this))
+  }
+  unit(){
+    let invLenght = 1.0 / this.size()
+    return this.mult(invLenght);
+  }
+  dif (v2){
+    return new Vec2(this.x-v2.x,this.y-v2.y);
+  }
+  decompose(n){
+    let nu = n.unit()
+    let vn = n.mult(this.dot(nu))
+    return [vn,this.dif(vn)]
+  }
+  reaction(n,alfa,beta){
+    let [vn,vp] = this.decompose(n)
+    let rx = alfa*vp.x - beta*vn.x
+    let ry = alfa*vp.y - beta*vn.y
+    return new Vec2(rx,ry)
+  }
+}
+/// guardam a posição do mouse no plano cartesiano
+var mouseXC, mouseYC = 0
+
+function setup(){
+  createCanvas(400,400)  
+}
+
+function draw(){
+  goCartesian()  
+  /*
+  colore(196, 128) 
+  var R = 128
+  for( ang=0; ang<2*PI; ang += PI/36)
+    seta(0,0, R*cos(ang), R*sin(ang))
+  */
+  colore(128,128,128)
+  rect(-200,94,400,10)
+  let vmouse = new Vec2(0,0)
+  let n = new Vec2(0,-30);
+  n.pos = new Vec2(-100,94);
+  n.cor = [255,0,0]
+  n.render()
+  vmouse.x = mouseXC
+  vmouse.y = mouseYC
+  vmouse.cor = [0,0,128]
+  vmouse.render()
+  let [vn,vp] = vmouse.decompose(n)
+  vn.cor = [128,128,128]
+  vp.cor = [128,128,128]
+  vn.render();
+  vp.render();
+  let alfa = 0.8;
+  let beta = 1;
+  let r = vmouse.reaction(n,alfa,beta);
+  r.cor = [128,0,128]
+  r.render()
+}
+
+function goCartesian()
+{
+  background(255)
+  
+  mouseXC = mouseX - width/2
+  mouseYC = height/2 - mouseY
+  
+  colore(128,0,0)
+  seta(0,height/2,width, height/2)
+  colore(0,128,0)
+  seta(width/2,height,width/2, 0)
+  
+  translate(width/2,height/2)
+  scale(1,-1,1)  
+}
+
+/// Atualiza as variáveis globais com as coordenadas do mouse no plano cartesiano
+function grabMouse()
+{
+  mouseXC = mouseX - width/2
+  mouseYC = height/2 - mouseY
+}
+
+/** Renderiza texto corretamente no plano cartesiano
+ *  @param str Texto a ser escrito
+ *  @param x Posição horizontal do canto inferior esquerdo texto
+ *  @param y Posição vertical do canto inferior esquerdo texto
+ */
+function texto(str,x,y)
+{
+  push()
+    translate( x, y)
+    scale(1,-1)
+    translate(-x,-y)
+  
+    // desenha o texto normalmente
+    text(str,x,y)
+  pop()
+}
+
+
+/* Define as cores de preenchimento e de contorno com o mesmo valor.
+ * Há várias opções de trabalho em RGB nesse caso:
+ *  - caso c1,c2,c3 e c4 sejam passados, o efeito padrão é uma cor RGBA
+ *  - caso c1,c2 e c3 sejam passados, tem-se uma cor RGB.
+ *  - caso c1 e c2 sejam passados, c1 é um tom de cinza e c2 é opacidade.
+ *  - caso apenas c1 seja passado, c1 é um tom de cinza.
+ */
+function colore(c1,c2,c3,c4)
+{
+  if(c4 != null)
+  {
+    fill(c1,c2,c3,c4)
+    stroke(c1,c2,c3,c4)
+    return
+  }
+  if(c3 != null)
+  {
+    fill(c1,c2,c3)
+    stroke(c1,c2,c3)
+    return
+  }
+  
+  if(c2 == null )
+  {
+    fill(c1)
+    stroke(c1)
+  }
+  else
+  {
+    fill(c1,c1,c1,c2)
+    stroke(c1,c1,c1,c2)
+  }    
+}
+
+/* Desenha um segmento de reta com seta do ponto (x1,y1) para (x2,y2)
+ */
+function seta(x1,y1,x2,y2)
+{
+  // o segmento de reta
+  line(x1,y1,x2,y2)
+  var dx = x2-x1, dy = y2-y1
+  var le = sqrt(dx*dx + dy*dy) // comprimento do vetor
+  // o vetor v é unitário paralelo ao segmento, com mesmo sentido
+  var vx = dx/le, vy = dy/le
+  // o vetor u é unitário e perpendicular ao segmento
+  var ux = -vy
+  var uy = vx
+  // a cabeça triangular
+  triangle(x2,y2,
+           x2-5*vx+2*ux, y2-5*vy+2*uy,
+           x2-5*vx-2*ux, y2-5*vy-2*uy)
+}
