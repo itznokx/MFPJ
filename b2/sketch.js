a68bfef942eb5994998f94353079a228317aa9d9
itznokx <dimitri.b.medeiros@gmail.com>
2025-02-06 12:08:52 -0300

    b2 start

---
+++ b2/sketch.js
@@ -0,0 +1,71 @@
+class Vec2{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.pos = null;
+    this.cor = null;
+  }
+  render (){
+    if (this.cor != null)
+      colore(this.cor[0],this.cor[1],this.cor[2])
+    if (this.pos == null)
+      seta(0,0,this.x,this.y)
+    else
+      seta(this.pos.x,
+           this.pos.y,
+           this.pos.x+this.x,
+           this.pos.y+this.y);
+  }
+  add (v2){
+    return new Vec2(this.x+v2.x,this.y+v2.y)
+  }
+  sum (v2){
+    return new Vec2(this.x+v2.x,this.y+v2.y)
+  }
+  dif (v2){
+    return new Vec2(this.x - v2.x,this.y - v2.y);
+  }
+  mult (k){
+    return new Vec2(this.x*k,this.y*k);
+  }
+  // Produto Escalar (dot product)
+  dot (v2){
+    let scalar = this.x*v2.x+this.y*v2.y
+    return scalar
+  }
+  size(){
+    return sqrt(this.dot(this))
+  }
+  normalize(){
+    let invLenght = 1.0 / this.size()
+    return this.mult(invLenght);
+  }
+  cross (v2){
+    let prod = this.x*v2.y - this.y*v2.x
+    return prod;
+  }
+  projection(n){
+    let v = new Vec2(this.x,this.y)
+    let num = v.dot(n);
+    let den = n.dot(n);
+    let div = num/den
+    let vn = n.mult(div)
+    let vp = v.dif(vn)
+    return [vn, vp]
+  }
+  reflection(n,alfa,beta){
+    let [vn,vp] = this.projection(n)
+    let rx = alfa*vp.x - beta*vn.x
+    let ry = alfa*vp.y - beta*vn.y
+    return new Vec2(rx,ry)
+  }
+  rot90(){
+    return new Vec2 (this.y,(-1)*this.x);
+  }
+}
+function setup(){
+  createCanvas(400,400)
+}
+function draw(){
+  
+}
